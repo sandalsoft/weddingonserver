6a23955c263da2aece554ed3cd42bf314c82b6b1
Eric Nelson <eric@sandalsoft.com>
2012-01-27 22:32:38 +0000

    added couple options to run script
    - renamed uploadLocation to updateLocation

--- run_post.bash
+++ run_post.bash
@@ -1,3 +1,7 @@
 
+## updateLocation
+#curl -d "uuid=0004fay0a8hoiy80&lat=41.999999&lon=-99.99999" http://wedding.sandalsoft.com/weddingonserver/updateLocation.php
 
-curl -d "uuid=0004fay0a8hoiy80&lat=41.999999&lon=-99.99999" http://wedding.sandalsoft.com/weddingonserver/uploadLocation.php
+
+## getGuestList
+curl -d "uuid=203F49E8-B247-5311-A2EE-705FA892DA2C" http://wedding.sandalsoft.com/weddingonserver/getGuestList.php
