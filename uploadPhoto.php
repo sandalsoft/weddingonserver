6ae32684bcd3914afce8be4a555efe2ffbe7703a
Eric Nelson <eric@sandalsoft.com>
2012-02-03 10:29:13 -0600

    cleaned up... testing git integration Signed-off-by:Eric Nelson <eric@sandalsoft.com>

--- uploadPhoto.php
+++ uploadPhoto.php
@@ -16,7 +16,6 @@ if (!$doc) {
    $images_dir =  "photos/images/album/";
    $thumbs_dir = "photos/thumbs/album/";
    
-   $uploaded_file = $_FILES["file"]["name"];
    $temp_filename = $_FILES["file"]["tmp_name"];
    $permanaent_filename = md5_file($temp_filename) . ".PNG";
   
