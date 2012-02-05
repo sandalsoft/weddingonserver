ee0cdbc87ec0d67811540083c327aaec162841c0
Eric Nelson <eric@sandalsoft.com>
2012-02-05 09:39:15 -0600

    Added mongohq as data source

--- getGuestList.php
+++ getGuestList.php
@@ -8,18 +8,36 @@ include_once "AuthUUID.php";
             header('HTTP/1.0 401 Unauthorized');
     }
     else {
-        $m = new Mongo();
+        $m = new Mongo("mongodb://mongohqdba:ilikebigtits@staff.mongohq.com:10004/weddingonsand");
         $collection = $m->weddingonsand->Persons;
-        
+
         //echo "AUTH OK";
         $cursor = $collection->find();
-        header('Content-Type: text/javascript');
-
-     foreach ($cursor as $id => $value) {
-         
-        echo json_encode( $value );
-}
-      
- 
+        header('Content-Type: application/json');
+        echo "{\"Persons\":[";
+         foreach ($cursor as $id => $value) {
+            echo "{";
+            echo "\"_id\":" . "\""   . $id . "\",";
+            echo "\"address\":"     .  "\"" . $value['address'] . "\",";
+            echo "\"app_auth_code\":"     .  "\"" . $value['app_auth_code'] . "\",";
+            echo "\"email\":"       . "\"" .  $value['email'] . "\",";
+            echo "\"facebook_url\":"       . "\"" .  $value['facebook_url'] . "\",";
+            echo "\"first_name\":"       . "\"" .  $value['first_name'] . "\",";
+            echo "\"has_app\":"     .  $value['has_app'] . ",";
+            echo "\"is_attending\":"    . $value['is_attending'] . ",";
+            echo "\"is_sharing_location\":"    . $value['is_sharing_location'] . ",";
+            echo "\"last_name\":"   .  "\"" . $value['last_name'] . "\",";
+            echo "\"latitude\":"   .  $value['latitude'] . ",";
+            echo "\"longitude\":"   .   $value['longitude'] . ",";
+            echo "\"phone_number\":"   .  "\"" . $value['phone_numer'] . "\",";
+            echo "\"twitter_handle\":"   .  "\"" . $value['twitter_handle'] . "\",";
+            echo "\"uuid\":"        .  "\"" . $value['uuid'] . "\",";
+            echo "\"wedding_role\":"   .  "\"" . $value['wedding_role'] . "\",";
+            echo "}";
+            if ($cursor->hasNext()) {
+                echo  ",";
+            }
+    }
+    echo "]}";  
     }
 ?>
