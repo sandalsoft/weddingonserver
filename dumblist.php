a1581a39bc60959556db16dab9ea7e220b3f2731
Eric Nelson <eric@sandalsoft.com>
2012-02-05 09:10:24 -0600

    changed perms

--- dumblist.php
+++ dumblist.php
@@ -15,18 +15,18 @@
      foreach ($cursor as $id => $value) {
         echo "{";
        // echo "\"Person\": {";
-        echo "\"id\":" . "\""   . $id . "\",";
+        echo "\"_id\":" . "\""   . $id . "\",";
         echo "\"address\":"     .  "\"" . $value['address'] . "\",";
         echo "\"app_auth_code\":"     .  "\"" . $value['app_auth_code'] . "\",";
         echo "\"email\":"       . "\"" .  $value['email'] . "\",";
         echo "\"facebook_url\":"       . "\"" .  $value['facebook_url'] . "\",";
         echo "\"first_name\":"       . "\"" .  $value['first_name'] . "\",";
-        echo "\"has_app\":"     .  "\"" . $value['has_app'] . "\",";
-        echo "\"is_attending\":"     .  "\"" . $value['is_attending'] . "\",";
-        echo "\"is_sharing_location\":"     .  "\"" . $value['is_sharing_location'] . "\",";
+        echo "\"has_app\":"     .  $value['has_app'] . ",";
+        echo "\"is_attending\":"    . $value['is_attending'] . ",";
+        echo "\"is_sharing_location\":"    . $value['is_sharing_location'] . ",";
         echo "\"last_name\":"   .  "\"" . $value['last_name'] . "\",";
-        echo "\"latitude\":"   .  "\"" . $value['latitude'] . "\",";
-        echo "\"longitude\":"   .  "\"" . $value['longitude'] . "\",";
+        echo "\"latitude\":"   .  $value['latitude'] . ",";
+        echo "\"longitude\":"   .   $value['longitude'] . ",";
         echo "\"phone_number\":"   .  "\"" . $value['phone_numer'] . "\",";
         echo "\"twitter_handle\":"   .  "\"" . $value['twitter_handle'] . "\",";
         echo "\"uuid\":"        .  "\"" . $value['uuid'] . "\",";
