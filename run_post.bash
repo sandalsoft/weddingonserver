e68322e3d70d67f1d09034aefcbe2de6eef3b427
Eric Nelson <eric@sandalsoft.com>
2012-02-07 14:50:10 -0600

    added test for doesFileExist

--- run_post.bash
+++ run_post.bash
@@ -4,6 +4,10 @@
 
 
 ## getGuestList
-curl -d "uuid=a26995db8cd00153e1c3c7055108942a1171b08d" http://wedding.sandalsoft.com/weddingonserver/getGuestList.php
+#curl -d "uuid=a26995db8cd00153e1c3c7055108942a1171b08d" http://wedding.sandalsoft.com/weddingonserver/getGuestList.php
 
 ## getPhotoGallrey
+
+
+## Check if image exists
+curl -d "uuid=a26995db8cd00153e1c3c7055108942a1171b08d&imageMD5=af4af4fe47849f495fc8c2ba3de6e51c" http://wedding.sandalsoft.com/weddingonserver/doesFileExist.php
