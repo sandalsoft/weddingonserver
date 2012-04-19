1bca2d1503f26e86cbf35632a8cb52f97f846e2e
Eric Nelson <eric@sandalsoft.com>
2012-04-19 13:19:04 -0500

    now returns json formatted metadata about uploaded images

--- showImages.php
+++ showImages.php
@@ -4,17 +4,27 @@
 $m = new Mongo("mongodb://mongouser:ilikebigtits@10.183.5.47:29317/weddingonsand");
 $db = $m->weddingonsand;    
 
+
 // GridFS
 $gridFS = $db->getGridFS();     
 
 // Find image to stream
-$images = $gridFS->find();
+$cursor = $gridFS->find();
 
-// Stream image to browser
-//header('Content-type: image/jpeg');
-header('Content-type: image/jpeg');
-foreach ($images["thumbnail"] as $thumb) {
-    echo ($thumb->getBytes());
+$response['images'] = NULL;
+$response['status']['status'] = "ok";
+
+foreach ($cursor as $filedata) {
+      $response['images'][] = $filedata;
 }
+
+print_r(json_encode($response));
+
+//// Stream image to browser
+////header('Content-type: image/jpeg');
+//header('Content-type: image/jpeg');
+//foreach ($images["thumbnail"] as $thumb) {
+//    echo ($thumb->getBytes());
+//}
 
 ?>
